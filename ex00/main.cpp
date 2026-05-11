b05413ba067857ed760078079f94a3a0cab5fbb3
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-05-11 18:11:55 +0100

    Editing ex00/main.cpp,

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,20 +1,36 @@
-
-
 #include <iostream>
-#include <map>
-#include <algorithm>
-#include <string>
+#include <exception>
+#include <stdexcept>
 
+class BadException : public std::exception
+{
+public:
+     const char *what() const throw()
+    {
+        std::cout << "Inside what(), now I will throw another exception..." << std::endl;
+
+        // throw std::runtime_error("Error thrown from what()");
+
+        return "This will never be returned";
+    }
+};
 
 int main()
 {
+    try
+    {
+        throw BadException();
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << "Caught BadException" << std::endl;
 
-  std::map<int , std::string> my_map;
-  char *end;
-   double a = std::strtod("123455",&end );
-  std::cout << a;
-  if(*end != '\0')
-    std::cout << "naaadi" << std::endl;
+        std::cout << e.what() << std::endl;
 
+        std::cout << "This line will probably not be printed" << std::endl;
+    }
 
-}     
+    std::cout << "End of program" << std::endl;
+
+    return 0;
+}
