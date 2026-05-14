ee0a528b8c77779376cc99c8685c895b9fb16072
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-05-14 17:23:10 +0100

    Editing ex01/RPN.cpp ex01/RPN.h ex01/main.cpp,

---
+++ ex01/RPN.h
@@ -0,0 +1,34 @@
+
+
+
+
+
+#ifndef RPN_H
+#define RPN_H
+
+
+#include <iostream>
+#include <stack>
+#include <exception>
+
+class RPN
+{
+    private :
+        std::stack<int> stack;
+    public:
+        RPN();
+        RPN(const RPN& other);
+        RPN & operator=(const RPN& other);
+        ~RPN();
+        class Error : public std::exception 
+        {
+            public:
+                const char *what() const throw() ;
+        };
+        int     calculate(const std::string &expression);
+
+};
+
+
+
+#endif
