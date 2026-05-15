5f4d478749ce18bfbf193c147f2139f174940d50
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-05-15 21:59:08 +0100

    Editing ex02/PmergeMe.cpp,

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -2,3 +2,30 @@
 
 
 #include "PmergeMe.h"
+
+
+PmergeMe::PmergeMe()
+{
+
+}
+PmergeMe::PmergeMe(const PmergeMe& other) : vec(other.vec) , deq(other.deq)
+{
+}
+
+PmergeMe & PmergeMe::operator=(const PmergeMe& other)
+{
+    if(this != &other)
+    {
+        vec = other.vec;
+        deq = other.deq;
+    }
+    return *this;
+}
+
+PmergeMe::~PmergeMe()
+{
+}
+
+
+
+
