eb547574695d47f6667e00c1b345fc649df76099
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-05-17 10:18:56 +0100

    Editing ex02/PmergeMe.cpp ex02/PmergeMe.h,

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -26,6 +26,63 @@ PmergeMe::~PmergeMe()
 {
 }
 
+void PmergeMe::parseInput(char **av)
+{
+    for (int i = 1; av[i]; ++i)
+    {
+        std::string arg(av[i]);
+        
+        
+        if (arg.empty() || arg.find_first_not_of(" \t") == std::string::npos)
+            throw std::invalid_argument("Error: Empty or whitespace-only argument.");
+
+        std::istringstream iss(arg);
+        std::string token;
+
+        while (iss >> token)
+        {
+
+            if (token.find_first_not_of("0123456789+") != std::string::npos)
+                throw std::invalid_argument("Error: Invalid character in sequence.");
+                
+
+            if (token.find('+') != std::string::npos && (token.find('+') != 0 || token.length() == 1))
+                throw std::invalid_argument("Error: Invalid formatting.");
+
+            long val = std::atol(token.c_str());
+            
+            if (val < 0 || val > 2147483647)
+                throw std::invalid_argument("Error: Number out of range.");
+
+            int intVal = static_cast<int>(val);
+
+           
+            if (std::find(vec.begin(), vec.end(), intVal) != vec.end())
+                throw std::invalid_argument("Error: Duplicate numbers are not allowed.");
+
+            
+            vec.push_back(intVal);
+            deq.push_back(intVal);
+        }
+    }
+}
 
 
+void PmergeMe::printVec(const std::string& message) const
+{
+    std::cout << message; 
+    
+    
+    size_t limit = vec.size() > 5 ? 5 : vec.size();
+
+    for (size_t i = 0; i < limit; ++i)
+    {
+        std::cout << vec[i] << " ";
+    }
+    
+    if (vec.size() > 5)
+        std::cout << "[...]"; 
+
+    std::cout << std::endl;
+}
 
--- ex02/PmergeMe.h
+++ ex02/PmergeMe.h
@@ -4,17 +4,20 @@
 #ifndef PmergeMe_H
 #define PmergeMe_H
 
-
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <exception>
 #include <deque>
-
+#include <sstream>
 class PmergeMe
 {
     private:
         std::vector<int> vec;
         std::deque<int> deq;
+        
+        void fordJohnsonVector(std::vector<int>& arr);
+        void fordJohnsonDeque(std::deque<int>& arr);
     public:
         PmergeMe();
         PmergeMe(const PmergeMe& other);
@@ -24,7 +27,8 @@ class PmergeMe
         void    parseInput(char **av);
         void    sortVec();
         void    sortDeq();
-        
+
+        void    printVec(const std::string& message) const;
 };
 
 
